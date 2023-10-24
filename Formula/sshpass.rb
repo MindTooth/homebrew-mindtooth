4ec0b7be44582c9af3e328dafa571b034b1811ff
Birger J. Nordølum <contact@mindtooth.no>
2023-10-24 10:25:06 +0200

    feat(sshpass): add head field

--- Formula/sshpass.rb
+++ Formula/sshpass.rb
@@ -3,6 +3,7 @@ class Sshpass < Formula
   homepage "https://sourceforge.net/projects/sshpass/"
   url "https://downloads.sourceforge.net/project/sshpass/sshpass/1.10/sshpass-1.10.tar.gz"
   sha256 "ad1106c203cbb56185ca3bad8c6ccafca3b4064696194da879f81c8d7bdfeeda"
+  head "https://git.code.sf.net/p/sshpass/code-git sshpass-code-git", branch: "main"
 
   def install
     system "./configure", "--disable-debug",
