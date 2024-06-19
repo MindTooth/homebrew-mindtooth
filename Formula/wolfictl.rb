a6a7e2408759c73b0612d28835f1595cb58c41d0
Birger J. Nordølum <contact@mindtooth.no>
2024-06-19 12:02:20 +0200

    test: add test to wolfictl

--- Formula/wolfictl.rb
+++ Formula/wolfictl.rb
@@ -22,6 +22,67 @@ class Wolfictl < Formula
   end
 
   test do
-    system bin/"wolfictl"
+    (testpath/"test.yaml").write <<~EOS
+      package:
+        name: melange
+        version: 0.9.0
+        epoch: 0
+        description: build APKs from source code
+        copyright:
+          - license: Apache-2.0
+        dependencies:
+          runtime:
+            - bubblewrap
+            - ca-certificates-bundle
+
+      environment:
+        contents:
+          packages:
+            - busybox
+            - ca-certificates-bundle
+            - go
+
+      pipeline:
+        - uses: git-checkout
+          with:
+            expected-commit: 2923188101d8fb20495e119850ad266d865cb8cd
+            repository: https://github.com/chainguard-dev/melange
+            tag: v${{package.version}}
+
+        - runs: |
+            make melange
+            install -m755 -D ./melange "${{targets.contextdir}}"/usr/bin/melange
+
+        - uses: strip
+
+      update:
+        enabled: true
+        github:
+          identifier: chainguard-dev/melange
+          strip-prefix: v
+          use-tag: true
+
+      test:
+        environment:
+          contents:
+            packages:
+              - apk-tools
+        pipeline:
+          - runs: |
+              apk del wolfi-base busybox
+              melange version
+
+              # busybox is needed for the build, but we want to demonstrate
+              # that it's not needed at runtime.
+              # TODO(jason): Can't currently test this due to container inception nonsense that's just too crazy to justify.
+              # melange build example.yaml --arch=x86_64
+    EOS
+
+    assert_equal "found 1 packages\n", shell_output("#{bin}/wolfictl lint #{testpath}/test.yaml")
+
+    # system bin/"melange", "keygen"
+    # assert_predicate testpath/"melange.rsa", :exist?
+
+    assert_match version.to_s, shell_output(bin/"wolfictl version 2>&1")
   end
 end
